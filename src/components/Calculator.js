45e38bcdab3ea7f314c882b8d771fcaa8615f34b
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-09-14 15:30:55 -0600

    Updates

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,63 +1,43 @@
 import '../App.css';
 import React from 'react';
+import ShowText from './showText';
 
-class ShowText extends React.Component {
+class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { result: 0 };
+    this.state = {};
   }
 
   render() {
-    const { result } = this.state;
     return (
-      <div className="showData">
-        <p>
-          {' '}
-          {result}
-          {' '}
-        </p>
+      <div id="main" className="main">
+        <ShowText />
+        <div id="operations" className="operations">
+          <div id="gridDiv" className="grid">
+            <tr className="calComp">AC</tr>
+            <tr className="calComp">+/-</tr>
+            <tr className="calComp">%</tr>
+            <tr className="opeComp">%</tr>
+            <tr className="calComp">7</tr>
+            <tr className="calComp">8</tr>
+            <tr className="calComp">9</tr>
+            <tr className="opeComp">*</tr>
+            <tr className="calComp">4</tr>
+            <tr className="calComp">5</tr>
+            <tr className="calComp">6</tr>
+            <tr className="opeComp">-</tr>
+            <tr className="calComp">1</tr>
+            <tr className="calComp">2</tr>
+            <tr className="calComp">3</tr>
+            <tr className="opeComp">+</tr>
+            <tr className="calComp num0">0</tr>
+            <tr className="calComp">.</tr>
+            <tr className="opeComp">=</tr>
+          </div>
+        </div>
       </div>
     );
   }
-}
-
-const GridDiv = () => (
-  <div id="gridDiv" className="grid">
-    <tr className="calComp">AC</tr>
-    <tr className="calComp">+/-</tr>
-    <tr className="calComp">%</tr>
-    <tr className="opeComp">+</tr>
-    <tr className="calComp">7</tr>
-    <tr className="calComp">8</tr>
-    <tr className="calComp">9</tr>
-    <tr className="opeComp">*</tr>
-    <tr className="calComp">4</tr>
-    <tr className="calComp">5</tr>
-    <tr className="calComp">6</tr>
-    <tr className="opeComp">-</tr>
-    <tr className="calComp">1</tr>
-    <tr className="calComp">2</tr>
-    <tr className="calComp">3</tr>
-    <tr className="opeComp">+</tr>
-    <tr className="calComp num0">0</tr>
-    <tr className="calComp">.</tr>
-    <tr className="opeComp">=</tr>
-  </div>
-);
-
-const DivOperations = () => (
-  <div id="operations" className="operations">
-    <GridDiv />
-  </div>
-);
-
-function Calculator() {
-  return (
-    <div id="main" className="main">
-      <ShowText />
-      <DivOperations />
-    </div>
-  );
 }
 
 export default Calculator;
